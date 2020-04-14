a8f58e0316978fbb3230d8d6e028f32250a37b5f
wingmar <sleepy.kroto@gmail.com>
2020-04-14 03:39:00 +0100

    added more constructors and tests

--- budget/src/main/java/com/wingmar/taffybear/budget/Transactions.java
+++ budget/src/main/java/com/wingmar/taffybear/budget/Transactions.java
@@ -4,10 +4,17 @@ import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -24,24 +31,61 @@ public class Transactions {
         this.transactions = transactions;
     }
 
-    public static Transactions createFromList(Collection<Transaction> transactions) {
+    public static Transactions of(Collection<Transaction> transactions) {
         return new Transactions(transactions);
     }
 
-    public static Transactions parseFromFile(File file) throws IOException {
-        try (final CSVParser parser = CSVFormat.EXCEL
-                .withFirstRecordAsHeader()
-                .parse(new FileReader(file))) {
-            final List<CSVRecord> records = parser.getRecords();
-            return createFromList(records.stream().map(record -> Transaction
-                    .createUsdTransaction(record.get(Header.DESCRIPTION.getName()),
-                            LocalDate.parse(record.get(Header.TRANSACTION_DATE.getName()),
-                                    DateTimeFormatter.ofPattern("MM/dd/yyyy")),
-                            BigDecimal.valueOf(Double.parseDouble(record.get(Header.AMOUNT.getName()))),
-                            record.get(Header.CATEGORY.getName()),
-                            TransactionType.fromName(record.get(Header.TYPE.getName()))))
-                    .collect(Collectors.toList()));
+    public static Transactions fromInputStream(InputStream inputStream, boolean includeHeaders) throws IOException {
+        return readTransactions(new InputStreamReader(inputStream), includeHeaders);
+    }
+
+    public static Transactions fromFile(File file, boolean includeHeaders) throws IOException {
+        return readTransactions(new FileReader(file), includeHeaders);
+    }
+
+    private static Transactions readTransactions(Reader reader, boolean includeHeaders) throws IOException {
+        if (includeHeaders) {
+            try (final CSVParser parser = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(reader)) {
+                final List<CSVRecord> records = parser.getRecords();
+                return parseRecordsWithHeaders(records);
+            }
+        } else {
+            try (final CSVParser parser = CSVFormat.EXCEL.parse(reader)) {
+                final List<CSVRecord> records = parser.getRecords();
+                return parseRecordsWithoutHeaders(records);
+            }
         }
+    }
+
+    private static Transactions parseRecordsWithHeaders(List<CSVRecord> records) {
+        return of(records.stream().map(record -> Transaction
+                .createUsdTransaction(record.get(Header.DESCRIPTION.getName()),
+                        LocalDate.parse(record.get(Header.TRANSACTION_DATE.getName()),
+                                DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+                        BigDecimal.valueOf(Double.parseDouble(record.get(Header.AMOUNT.getName()))),
+                        record.get(Header.CATEGORY.getName()),
+                        TransactionType.fromName(record.get(Header.TYPE.getName()))))
+                .collect(Collectors.toList()));
+    }
+
+    private static Transactions parseRecordsWithoutHeaders(List<CSVRecord> records) {
+        final List<Transaction> list = records.stream().map(record -> {
+            final String merchant = record.get(Header.DESCRIPTION.ordinal());
+            final LocalDate transactionDate = LocalDate.parse(record.get(Header.TRANSACTION_DATE.ordinal()),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            final BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(record.get(Header.AMOUNT.ordinal())));
+            final String category = record.get(Header.CATEGORY.ordinal());
+            final TransactionType type = TransactionType.fromName(record.get(Header.TYPE.ordinal()));
+            return Transaction
+                    .createUsdTransaction(merchant,
+                            transactionDate,
+                            amount,
+                            category,
+                            type);
+        })
+                .collect(Collectors.toList());
+        return of(list);
+
     }
 
     private enum Header {
@@ -60,6 +104,25 @@ public class Transactions {
         public String getName() {
             return name;
         }
+
+    }
+
+    public InputStream asInputStream() throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(out), CSVFormat.EXCEL)) {
+            transactions.forEach(transaction -> {
+                try {
+                    printer.printRecord(transaction.getDate(),
+                            transaction.getMerchant(),
+                            transaction.getCategory(),
+                            transaction.getType().getName(),
+                            transaction.getAmount().getAmount());
+                } catch (IOException e) {
+                    throw new IllegalStateException(e);
+                }
+            });
+        }
+        return new ByteArrayInputStream(out.toByteArray());
     }
 
     public List<Transaction> asList() {
--- budget/src/test/java/com/wingmar/taffybear/budget/TransactionsTest.java
+++ budget/src/test/java/com/wingmar/taffybear/budget/TransactionsTest.java
@@ -3,7 +3,9 @@ package com.wingmar.taffybear.budget;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
@@ -16,10 +18,10 @@ public class TransactionsTest {
 
     @Test
     public void equals() {
-        final Transactions transactions = Transactions.createFromList(Collections.singletonList(Transaction
+        final Transactions transactions = Transactions.of(Collections.singletonList(Transaction
                 .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
                         BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
-        final Transactions other = Transactions.createFromList(Collections.singletonList(Transaction
+        final Transactions other = Transactions.of(Collections.singletonList(Transaction
                 .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
                         BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
 
@@ -28,10 +30,10 @@ public class TransactionsTest {
 
     @Test
     public void hash() {
-        final Transactions transactions = Transactions.createFromList(Collections.singletonList(Transaction
+        final Transactions transactions = Transactions.of(Collections.singletonList(Transaction
                 .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
                         BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
-        final Transactions other = Transactions.createFromList(Collections.singletonList(Transaction
+        final Transactions other = Transactions.of(Collections.singletonList(Transaction
                 .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
                         BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
 
@@ -44,7 +46,7 @@ public class TransactionsTest {
         final File file = new File(getClass().getResource("transactions.csv").toURI());
 
         // when
-        final Transactions transactions = Transactions.parseFromFile(file);
+        final Transactions transactions = Transactions.fromFile(file, true);
 
         // then
         final Transaction t0 = Transaction.createUsdTransaction("FIRSTENERGY/EZPAYRECUR",
@@ -62,12 +64,54 @@ public class TransactionsTest {
                 BigDecimal.valueOf(-344.85),
                 "Home",
                 TransactionType.SALE);
-        assertThat(transactions, is(Transactions.createFromList(ImmutableList.of(t0, t1, t2))));
+        assertThat(transactions, is(Transactions.of(ImmutableList.of(t0, t1, t2))));
+    }
+
+    @Test
+    public void fromInputStream() throws IOException, URISyntaxException {
+        // given
+
+        // when
+        final Transactions transactions = Transactions.fromInputStream(new FileInputStream(new File(getClass()
+                .getResource("transactions.csv").toURI())), true);
+
+        // then
+        final Transaction t0 = Transaction.createUsdTransaction("FIRSTENERGY/EZPAYRECUR",
+                LocalDate.of(2020, 3, 30),
+                BigDecimal.valueOf(-66.28),
+                "Bills & Utilities",
+                TransactionType.SALE);
+        final Transaction t1 = Transaction.createUsdTransaction("CHEWY.COM",
+                LocalDate.of(2020, 3, 30),
+                BigDecimal.valueOf(35.28),
+                "Shopping",
+                TransactionType.RETURN);
+        final Transaction t2 = Transaction.createUsdTransaction("VONEIFF OIL",
+                LocalDate.of(2020, 3, 23),
+                BigDecimal.valueOf(-344.85),
+                "Home",
+                TransactionType.SALE);
+        assertThat(transactions, is(Transactions.of(ImmutableList.of(t0, t1, t2))));
+    }
+
+    @Test
+    public void asInputStream() throws IOException {
+        // given
+        final Transactions transactions = Transactions.of(Collections.singletonList(Transaction
+                .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
+                        BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
+
+        // when
+        final InputStream actual = transactions.asInputStream();
+        final Transactions fromInputStream = Transactions.fromInputStream(actual, false);
+
+        // then
+        assertThat(fromInputStream, is(transactions));
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void asList_immutable() {
-        final Transactions transactions = Transactions.createFromList(Collections.singletonList(Transaction
+        final Transactions transactions = Transactions.of(Collections.singletonList(Transaction
                 .createUsdTransaction("merchant", LocalDate.of(2020, 12, 1),
                         BigDecimal.valueOf(2.5), "category", TransactionType.SALE)));
 
